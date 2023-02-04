e300930efd96b4537f2595db57e11efa5eab0a09
AnhLe3698 <anhviet.le00@hotmail.com>
2023-02-04 01:33:04 -0700

    Add instance variables

--- prac_ass/person.rb
+++ prac_ass/person.rb
@@ -1,10 +1,32 @@
+# class Person
+
+#   def initialize(name)
+#     @name = name
+#   end
+
+#   def name
+#     @name
+#   end
+
+#   def attr_writer(name)
+#     @name = name
+#   end
+
+# end
+
+# p = Person.new('L. Ron')
+# p.attr_writer('bob')
+# puts p.name
+
 class Person
+
+  attr_accessor :name
 
   def initialize(name)
     @name = name
   end
-
 end
 
 p = Person.new('L. Ron')
+p.name = "New Name"
 puts p.name
